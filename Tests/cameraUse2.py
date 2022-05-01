70e884c65336157b932dfcbc469381aec66f02c9
Jocker00 <grove1411@gmail.com>
2022-05-01 17:30:05 -0300

    ESP32-CAM format

---
+++ Tests/cameraUse2.py
@@ -0,0 +1,33 @@
+import camera
+
+## ESP32-CAM (default configuration) - https://bit.ly/2Ndn8tN
+print('Iniciando camera...')
+camera.init(0, format=camera.JPEG)
+print('Camera iniciada!\n')
+# The parameters: format=camera.JPEG, xclk_freq=camera.XCLK_10MHz are standard for all cameras.
+# You can try using a faster xclk (20MHz), this also worked with the esp32-cam and m5camera
+# but the image was pixelated and somehow green.
+
+
+
+
+
+# quality
+#camera.quality(63)
+# 10-63 lower number means higher quality
+
+print('Tirando foto...')
+buf = camera.capture()
+print(f"Foto tirada: {buf}\n")
+
+print('Salvando foto...')
+if len(buf): # Check if the photo is ok
+    imageFile = open(f"teste.jpeg", 'w')
+    imageFile.write(buf)
+    imageFile.close()
+
+    print("Photo taked!")
+    print(os.listdir())
+
+else:
+    print("Photo not taked, an error occurred.")
